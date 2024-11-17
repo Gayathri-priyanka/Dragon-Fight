c4cef904bdf605aee3835c351a50e1fa17f99e98
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2024-11-16 22:21:33 -0500

    Weapon functionality

--- script.js
+++ script.js
@@ -90,8 +90,12 @@ function buyWeapon() {
     gold -= 30;
     currentWeaponIndex++;
     goldText.innerText = gold;
-    
-    text.innerText = "You now have a new weapon.";
+    let newWeapon = weapons[currentWeaponIndex].name;
+    text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
   }
 }
 
